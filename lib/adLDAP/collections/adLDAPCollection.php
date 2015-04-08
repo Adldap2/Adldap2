a8a7ccbf057bd6f7250a3d98fc5858bb997c0dcd
Steve Bauman <steven_bauman@outlook.com>
2015-04-08 12:28:01 -0400

    Optimize collection class

--- lib/adLDAP/collections/adLDAPCollection.php
+++ lib/adLDAP/collections/adLDAPCollection.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace adLDAP\collections;
+
 use adLDAP\adLDAP;
+
 /**
  * PHP LDAP CLASS FOR MANIPULATING ACTIVE DIRECTORY 
  * Version 5.0.0
@@ -35,100 +38,127 @@ use adLDAP\adLDAP;
  * @version 5.0.0
  * @link http://github.com/adldap/adLDAP
  */
+
+/**
+ * Class adLDAPCollection
+ * @package adLDAP\collections
+ */
 abstract class adLDAPCollection
 {
     /**
-    * The current adLDAP connection via dependency injection
-    * 
-    * @var adLDAP
-    */
+     * The current adLDAP connection via dependency injection
+     *
+     * @var adLDAP
+     */
     protected $adldap;
-    
+
     /**
-    * The current object being modifed / called
-    * 
-    * @var mixed
-    */
+     * The current object being modified / called
+     *
+     * @var mixed
+     */
     protected $currentObject;
-    
+
     /**
-    * The raw info array from Active Directory
-    * 
-    * @var array
-    */
+     * The raw info array from Active Directory
+     *
+     * @var array
+     */
     protected $info;
-    
-    public function __construct($info, adLDAP $adldap) {
-        $this->setInfo($info);   
+
+    /**
+     * Constructor.
+     *
+     * @param $info
+     * @param adLDAP $adldap
+     */
+    public function __construct($info, adLDAP $adldap)
+    {
+        $this->setInfo($info);
+
         $this->adldap = $adldap;
     }
-    
+
     /**
-    * Set the raw info array from Active Directory
-    * 
-    * @param array $info
-    */
-    public function setInfo(array $info) {
-        if ($this->info && sizeof($info) >= 1) {
-            unset($this->info);
-        }
+     * Set the raw info array from Active Directory
+     *
+     * @param array $info
+     */
+    public function setInfo(array $info)
+    {
+        if ($this->info && sizeof($info) >= 1) unset($this->info);
+
         $this->info = $info;   
     }
-    
+
     /**
-    * Magic get method to retrieve data from the raw array in a formatted way
-    * 
-    * @param string $attribute
-    * @return mixed
-    */
-    public function __get($attribute) {
-        if (isset($this->info[0]) && is_array($this->info[0])) {
-            foreach ($this->info[0] as $keyAttr => $valueAttr) {
-                if (strtolower($keyAttr) == strtolower($attribute)) {
-                    if ($this->info[0][strtolower($attribute)]['count'] == 1) {
+     * Magic get method to retrieve data from the raw array in a formatted way
+     *
+     * @param string $attribute
+     * @return array|null
+     */
+    public function __get($attribute)
+    {
+        if (isset($this->info[0]) && is_array($this->info[0]))
+        {
+            foreach ($this->info[0] as $keyAttr => $valueAttr)
+            {
+                if (strtolower($keyAttr) == strtolower($attribute))
+                {
+                    if ($this->info[0][strtolower($attribute)]['count'] == 1)
+                    {
                         return $this->info[0][strtolower($attribute)][0];   
                     }
-                    else {
+                    else
+                    {
                         $array = array();
-                        foreach ($this->info[0][strtolower($attribute)] as $key => $value) {
-                            if ((string)$key != 'count') {
+
+                        foreach ($this->info[0][strtolower($attribute)] as $key => $value)
+                        {
+                            if ((string)$key != 'count')
+                            {
                                 $array[$key] = $value;
                             } 
-                        }  
+                        }
+
                         return $array;   
                     }
                 }   
             }
         }
-        else {
-            return NULL;   
-        }
+
+        return NULL;
     }    
-    
+
     /**
-    * Magic set method to update an attribute
-    * 
-    * @param string $attribute
-    * @param string $value
-    * @return bool
-    */
+     * Magic set method to update an attribute
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return mixed
+     */
     abstract public function __set($attribute, $value);
-    
-    /** 
-    * Magic isset method to check for the existence of an attribute 
-    * 
-    * @param string $attribute 
-    * @return bool 
-    */ 
-    public function __isset($attribute) {
-        if (isset($this->info[0]) && is_array($this->info[0])) { 
-            foreach ($this->info[0] as $keyAttr => $valueAttr) { 
-                if (strtolower($keyAttr) == strtolower($attribute)) { 
+
+    /**
+     * Magic isset method to check for the existence of an attribute
+     *
+     * @param $attribute
+     * @return bool
+     */
+    public function __isset($attribute)
+    {
+        if (isset($this->info[0]) && is_array($this->info[0]))
+        {
+            foreach ($this->info[0] as $keyAttr => $valueAttr)
+            {
+                if (strtolower($keyAttr) == strtolower($attribute))
+                {
                     return true; 
                 } 
             } 
-        } 
-        return false; 
-     } 
+        }
+
+        return false;
+    }
 }
 ?>

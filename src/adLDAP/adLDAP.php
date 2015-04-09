e612ce6d0e2acd97cf21a5b510ccde87c2145cfd
Steve Bauman <steven_bauman@outlook.com>
2015-04-09 12:45:50 -0400

    Merge license with class header

--- src/adLDAP/adLDAP.php
+++ src/adLDAP/adLDAP.php
@@ -2,40 +2,6 @@
 
 namespace adLDAP;
 
-/**
- * PHP LDAP CLASS FOR MANIPULATING ACTIVE DIRECTORY 
- * Version 5.0.0
- * 
- * PHP Version 5 with SSL and LDAP support
- * 
- * Written by Scott Barnett, Richard Hyland
- *   email: scott@wiggumworld.com, adldap@richardhyland.com
- *   http://github.com/adldap/adLDAP
- * 
- * Copyright (c) 2006-2014 Scott Barnett, Richard Hyland
- * 
- * We'd appreciate any improvements or additions to be submitted back
- * to benefit the entire community :)
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * @category ToolsAndUtilities
- * @package adLDAP
- * @author Scott Barnett, Richard Hyland
- * @copyright (c) 2006-2014 Scott Barnett, Richard Hyland
- * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPLv2.1
- * @version 5.0.0
- * @link http://github.com/adldap/adLDAP
- */
-
 require_once(dirname(__FILE__) . '/collections/adLDAPCollection.php');
 require_once(dirname(__FILE__) . '/classes/adLDAPGroups.php');
 require_once(dirname(__FILE__) . '/classes/adLDAPUsers.php');
@@ -46,6 +12,30 @@ require_once(dirname(__FILE__) . '/classes/adLDAPExchange.php');
 require_once(dirname(__FILE__) . '/classes/adLDAPComputers.php');
 
 /**
+ * PHP LDAP CLASS FOR MANIPULATING ACTIVE DIRECTORY
+ * Version 5.0.0
+ *
+ * PHP Version 5 with SSL and LDAP support
+ *
+ * Written by Scott Barnett, Richard Hyland
+ *   email: scott@wiggumworld.com, adldap@richardhyland.com
+ *   http://github.com/adldap/adLDAP
+ *
+ * Copyright (c) 2006-2014 Scott Barnett, Richard Hyland
+ *
+ * We'd appreciate any improvements or additions to be submitted back
+ * to benefit the entire community :)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
  * Main adLDAP class
  *
  * Can be initialised using $adldap = new adLDAP();
@@ -59,6 +49,8 @@ require_once(dirname(__FILE__) . '/classes/adLDAPComputers.php');
  *
  * Class adLDAP
  * @package adLDAP
+ * @author Scott Barnett, Richard Hyland
+ * @copyright (c) 2006-2014 Scott Barnett, Richard Hyland
  */
 class adLDAP
 {

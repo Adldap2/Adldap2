318bd173ba96b341864ccb99de965be1ed999230
Steve Bauman <steven_bauman@outlook.com>
2015-04-10 10:49:41 -0400

    Convert functional test case to abstract

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -4,7 +4,7 @@ namespace adLDAP\Tests;
 
 use Mockery;
 
-class FunctionalTestCase extends \PHPUnit_Framework_TestCase
+abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
 {
     protected function mock($class)
     {

d11d2fd0cce213e30e4e567669c4b3a22b1cb59b
Steve Bauman <steven_bauman@outlook.com>
2015-04-10 12:05:07 -0400

    Move test definitions into PHPUnit setUp method

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -4,19 +4,24 @@ namespace adLDAP\Tests;
 
 use Mockery;
 
-// Set constants for testing without LDAP support
-if( ! defined('LDAP_OPT_PROTOCOL_VERSION')) define('LDAP_OPT_PROTOCOL_VERSION', 3);
-
-if( ! defined('LDAP_OPT_REFERRALS')) define('LDAP_OPT_REFERRALS', 0);
-
-if( ! array_key_exists('REMOTE_USER', $_SERVER)) $_SERVER['REMOTE_USER'] = 'true';
-
-if( ! array_key_exists('KRB5CCNAME', $_SERVER)) $_SERVER['KRB5CCNAME'] = 'true';
-
 abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
 {
     protected function mock($class)
     {
         return Mockery::mock($class);
+    }
+
+    public function setUp()
+    {
+        // Set constants for testing without LDAP support
+        if( ! defined('LDAP_OPT_PROTOCOL_VERSION')) define('LDAP_OPT_PROTOCOL_VERSION', 3);
+
+        if( ! defined('LDAP_OPT_REFERRALS')) define('LDAP_OPT_REFERRALS', 0);
+
+        if( ! array_key_exists('REMOTE_USER', $_SERVER)) $_SERVER['REMOTE_USER'] = 'true';
+
+        if( ! array_key_exists('KRB5CCNAME', $_SERVER)) $_SERVER['KRB5CCNAME'] = 'true';
+
+        parent::setUp();
     }
 }

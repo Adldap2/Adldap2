e91eee38dc90aa5582ae0c63027a885ecb011f8c
Steve Bauman <steven_bauman@outlook.com>
2015-04-10 12:01:51 -0400

    Define server variables for testing

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -9,6 +9,10 @@ if( ! defined('LDAP_OPT_PROTOCOL_VERSION')) define('LDAP_OPT_PROTOCOL_VERSION', 
 
 if( ! defined('LDAP_OPT_REFERRALS')) define('LDAP_OPT_REFERRALS', 0);
 
+if( ! array_key_exists('REMOTE_USER', $_SERVER)) $_SERVER['REMOTE_USER'] = 'true';
+
+if( ! array_key_exists('KRB5CCNAME', $_SERVER)) $_SERVER['KRB5CCNAME'] = 'true';
+
 abstract class FunctionalTestCase extends \PHPUnit_Framework_TestCase
 {
     protected function mock($class)

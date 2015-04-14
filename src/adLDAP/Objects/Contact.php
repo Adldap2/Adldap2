c2cf9824529edf8acf8a0e4a1088df7dde7e9ff0
Steve Bauman <steven_bauman@outlook.com>
2015-04-13 21:07:56 -0400

    Added Contact object

---
+++ src/adLDAP/Objects/Contact.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace adLDAP\Objects;
+
+use adLDAP\Exceptions\adLDAPException;
+
+/**
+ * Class Contact
+ * @package adLDAP\Objects
+ */
+class Contact extends AbstractObject
+{
+    /**
+     * The required attributes.
+     *
+     * @var array
+     */
+    protected $required = array(
+        'display_name',
+        'email',
+        'container'
+    );
+
+    /**
+     * Validates the required attributes.
+     *
+     * @throws adLDAPException
+     */
+    public function validateRequired()
+    {
+        parent::validateRequired();
+
+        if ( ! is_array($this->getAttribute("container"))) throw new adLDAPException("Container attribute must be an array.");
+    }
+}

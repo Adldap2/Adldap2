a347d64cfc8a9a72a7263e78cc3371296b154ee8
Steve Bauman <steven_bauman@outlook.com>
2015-04-16 14:47:38 -0400

    Added some small test documentation

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -6,6 +6,10 @@ use Adldap\Connections\Ldap;
 
 class ConnectionTest extends FunctionalTestCase
 {
+    /**
+     * This tests that no exception is thrown when trying
+     * to perform an LDAP method while suppressing errors.
+     */
     public function testConnectionSuppressErrors()
     {
         $ldap = new Ldap();
@@ -15,6 +19,11 @@ class ConnectionTest extends FunctionalTestCase
         $this->assertFalse($ldap->bind('test', 'test'));
     }
 
+    /**
+     * This tests that an exception is thrown
+     * when trying to perform an LDAP method while
+     * showing errors.
+     */
     public function testConnectionShowErrors()
     {
         $ldap = new Ldap();

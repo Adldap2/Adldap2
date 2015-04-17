9b8749511bf093fc27310b5959d31c66ddd53115
Steve Bauman <steven_bauman@outlook.com>
2015-04-17 13:24:00 -0400

    More search methods

--- src/Adldap/Classes/AdldapSearch.php
+++ src/Adldap/Classes/AdldapSearch.php
@@ -2,6 +2,7 @@
 
 namespace Adldap\Classes;
 
+use Adldap\Adldap;
 use Adldap\Exceptions\AdldapException;
 use Adldap\Objects\LdapEntry;
 
@@ -31,7 +32,7 @@ class AdldapSearch extends AdldapBase
     );
 
     /**
-     * Stores the default fields to query
+     * Stores the default fields to query.
      *
      * @var array
      */
@@ -41,7 +42,18 @@ class AdldapSearch extends AdldapBase
         'displayname',
         'distinguishedname',
         'samaccountname',
+        "objectcategory",
+        "operatingsystem",
+        "operatingsystemservicepack",
+        "operatingsystemversion"
     );
+
+    /**
+     * Stores the selects to use in the query when assembled.
+     *
+     * @var array
+     */
+    protected $selects = array();
 
     /**
      * Stores the wheres to use in the query when assembled.
@@ -50,10 +62,18 @@ class AdldapSearch extends AdldapBase
      */
     protected $wheres = array();
 
-    protected $andWheres = array();
-
+    /**
+     * The opening query string.
+     *
+     * @var string
+     */
     protected static $openQuery = '(';
 
+    /**
+     * The closing query string.
+     *
+     * @var string
+     */
     protected static $closeQuery = ')';
 
     /**
@@ -63,53 +83,126 @@ class AdldapSearch extends AdldapBase
      */
     public function get()
     {
-        $this->query = $this->assembleQuery();
+        $this->assembleQuery();
 
-        $results = $this->connection->search($this->adldap->getBaseDn(), $this->query, $this->fields);
+        $results = $this->connection->search($this->adldap->getBaseDn(), $this->getQuery(), $this->getSelects());
 
-        if($results)
-        {
-            $entries = $this->connection->getEntries($results);
-
-            $objects = array();
-
-            for ($i = 0; $i < $entries["count"]; $i++)
-            {
-                $entry = new LdapEntry($entries[$i], $this->connection);
-
-                $objects[] = $entry->getAttributes();
-            }
-
-            return $objects;
-        }
+        if($results) return $this->processResults($results);
 
         return false;
     }
 
-    public function select()
+    /**
+     * Adds the inserted fields to query on the current LDAP connection.
+     *
+     * @param array $fields
+     * @return $this
+     */
+    public function select($fields = array())
     {
+        if(is_array($fields))
+        {
+            foreach($fields as $field)
+            {
+                $this->addSelect($field);
+            }
+        } else if(is_string($fields))
+        {
+            $this->addSelect($fields);
+        }
 
+        return $this;
     }
 
+    /**
+     * @param $field
+     * @param null $operator
+     * @param null $value
+     * @return $this
+     */
     public function where($field, $operator = null, $value = null)
     {
-        $operator = $this->getOperator($operator);
-
         $this->addWhere($field, $operator, $value);
 
         return $this;
     }
 
-    private function addSelect()
+    /**
+     * Returns true / false depending if the current object
+     * contains selects.
+     *
+     * @return bool
+     */
+    public function hasSelects()
+    {
+        if(count($this->selects) > 0) return true;
+
+        return false;
+    }
+
+    /**
+     * Returns the current selected fields to retrieve.
+     *
+     * @return array
+     */
+    public function getSelects()
+    {
+        $fields = $this->fields;
+
+        /*
+         * If the current search object has specific
+         * search fields, we'll use those instead.
+         */
+        if($this->hasSelects()) $fields = $this->selects;
+
+        return $fields;
+    }
+
+    /**
+     *
+     *
+     * @param $field
+     * @param string $direction
+     */
+    public function sortBy($field, $direction = 'desc')
     {
 
     }
 
-    private function addWhere($field, $operator, $value)
+    /**
+     * Returns the current LDAP query string.
+     *
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * Adds the inserted field to the selects property.
+     *
+     * @param string $field
+     */
+    private function addSelect($field)
+    {
+        $this->selects[] = $field;
+    }
+
+    /**
+     * Adds the inserted field, operator and value to the wheres
+     * property array.
+     *
+     * @param string $field
+     * @param string $operator
+     * @param null $value
+     * @throws AdldapException
+     */
+    private function addWhere($field, $operator, $value = null)
     {
         $this->wheres[] = array(
             'field' => $field,
-            'operator' => $operator,
+            'operator' => $this->getOperator($operator),
             'value' => $this->connection->escape($value),
         );
     }
@@ -121,18 +214,75 @@ class AdldapSearch extends AdldapBase
      */
     private function assembleQuery()
     {
-        $query = '';
-
-        foreach($this->wheres as $where)
+        if(count($this->wheres) > 0)
         {
-            $query .= $this::$openQuery
-                . $where['field']
-                . $where['operator']
-                . $where['value']
-                . $this::$closeQuery;
-        }
+            foreach($this->wheres as $where)
+            {
+                switch($where['operator'])
+                {
+                    case '=':
+                        $this->query .= $this->queryEquals($where['field'], $where['value']);
+                        break;
+                    case '!':
+                        $this->query .= $this->queryDoesNotEqual($where['field'], $where['value']);
+                        break;
+                    case '*':
+                        $this->query .= $this->queryWildcard($where['field']);
+                        break;
+                }
+            }
 
-        return $query;
+            if(count($this->wheres) > 1)
+            {
+                $this->query = $this->queryAnd($this->query);
+            }
+        }
+    }
+
+    /**
+     * Returns a query string for does not equal.
+     *
+     * @param string $field
+     * @param string $value
+     * @return string
+     */
+    private function queryDoesNotEqual($field, $value)
+    {
+        return $this::$openQuery . '!' . $this->queryEquals($field, $value) . $this::$closeQuery;
+    }
+
+    /**
+     * Returns a query string for equals.
+     *
+     * @param string $field
+     * @param string $value
+     * @return string
+     */
+    private function queryEquals($field, $value)
+    {
+        return $this::$openQuery . $field . '=' . $value . $this::$closeQuery;
+    }
+
+    /**
+     * Returns a query string for a wildcard.
+     *
+     * @param string $field
+     * @return string
+     */
+    private function queryWildcard($field)
+    {
+        return $this::$openQuery .  $field . '=*' . $this::$closeQuery;
+    }
+
+    /**
+     * Wraps the inserted query inside an AND operator.
+     *
+     * @param $query
+     * @return string
+     */
+    private function queryAnd($query)
+    {
+        return $this::$openQuery . '&' . $query . $this::$closeQuery;
     }
 
     /**
@@ -148,12 +298,34 @@ class AdldapSearch extends AdldapBase
     {
         $key = array_search($operator, $this->operators);
 
-        if($key) return $this->operators[$key];
+        if(array_key_exists($key, $this->operators)) return $this->operators[$key];
 
-        $operators = implode(',', $this->operators);
+        $operators = implode(', ', $this->operators);
 
         $message = "Operator: $operator cannot be used in an LDAP query. Available operators are $operators";
 
         throw new AdldapException($message);
+    }
+
+    /**
+     * Processes LDAP search results into a nice array.
+     *
+     * @param resource $results
+     * @return array
+     */
+    private function processResults($results)
+    {
+        $entries = $this->connection->getEntries($results);
+
+        $objects = array();
+
+        for ($i = 0; $i < $entries["count"]; $i++)
+        {
+            $entry = new LdapEntry($entries[$i], $this->connection);
+
+            $objects[] = $entry->getAttributes();
+        }
+
+        return $objects;
     }
 }

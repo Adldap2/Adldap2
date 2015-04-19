5ab65c48a949df02e3fd1b98ea4ce2bed87d64e5
Steve Bauman <steven_bauman@outlook.com>
2015-04-19 17:15:43 -0400

    Organized and added more LDAP entry attributes

--- src/Adldap/Objects/LdapEntry.php
+++ src/Adldap/Objects/LdapEntry.php
@@ -40,6 +40,23 @@ class LdapEntry extends AbstractObject
      */
     public function applyAttributes($attributes)
     {
+        // Apply generic attributes
+        $this->applyGenericAttributes($attributes);
+
+        // Apply user attributes
+        $this->applyUserAttributes($attributes);
+
+        // Apply computer attributes
+        $this->applyComputerAttributes($attributes);
+    }
+
+    /**
+     * Applies the LDAP entry's generic attributes.
+     *
+     * @param array $attributes
+     */
+    public function applyGenericAttributes(array $attributes = array())
+    {
         // Does the entry contain a common name?
         if(array_key_exists('cn', $attributes))
         {
@@ -50,30 +67,6 @@ class LdapEntry extends AbstractObject
         if(array_key_exists('description', $attributes))
         {
             $this->setAttribute('description', $attributes['description'][0]);
-        }
-
-        // Does the entry contain a display name?
-        if(array_key_exists('displayname', $attributes))
-        {
-            $this->setAttribute('displayname', $attributes['displayname'][0]);
-        }
-
-        // Does the entry contain a logon name?
-        if(array_key_exists('samaccountname', $attributes))
-        {
-            $this->setAttribute('samaccountname', $attributes['samaccountname'][0]);
-        }
-
-        // Does the entry contain
-        if(array_key_exists('mail', $attributes))
-        {
-            $this->setAttribute('mail', $attributes['mail'][0]);
-        }
-
-        // Does the entry contain an object category?
-        if(array_key_exists('objectcategory', $attributes))
-        {
-            $this->setAttribute('objectcategory', $attributes['objectcategory'][0]);
         }
 
         // Does the entry contain a distinguished name?
@@ -88,12 +81,71 @@ class LdapEntry extends AbstractObject
             $this->setAttribute('dn_array', $this->connection->explodeDn($dn, true));
         }
 
-        // Apply the extra attributes
-        $this->applyComputerAttributes($attributes);
+        // Does the entry contain an object category?
+        if(array_key_exists('objectcategory', $attributes))
+        {
+            $this->setAttribute('objectcategory', $attributes['objectcategory'][0]);
+        }
+
+        if(array_key_exists('info', $attributes))
+        {
+            $this->setAttribute('info', $attributes['info'][0]);
+        }
     }
 
     /**
-     * Applies the objects attributes from the specified array.
+     * Applies the entry's user attributes.
+     *
+     * @param array $attributes
+     */
+    public function applyUserAttributes(array $attributes = array())
+    {
+        // Does the entry contain a display name?
+        if(array_key_exists('displayname', $attributes))
+        {
+            $this->setAttribute('displayname', $attributes['displayname'][0]);
+        }
+
+        if(array_key_exists('displayNamePrintable', $attributes))
+        {
+            $this->setAttribute('displayNamePrintable', $attributes['displayNamePrintable'][0]);
+        }
+
+        // Does the entry contain a logon name?
+        if(array_key_exists('samaccountname', $attributes))
+        {
+            $this->setAttribute('samaccountname', $attributes['samaccountname'][0]);
+        }
+
+        if(array_key_exists('userPrincipalName', $attributes))
+        {
+            $this->setAttribute('userPrincipalName', $attributes['userPrincipalName'][0]);
+        }
+
+        if(array_key_exists('initials', $attributes))
+        {
+            $this->setAttribute('initials', $attributes['initials'][0]);
+        }
+
+        // Does the entry contain
+        if(array_key_exists('mail', $attributes))
+        {
+            $this->setAttribute('mail', $attributes['mail'][0]);
+        }
+
+        if(array_key_exists('company', $attributes))
+        {
+            $this->setAttribute('company', $attributes['company'][0]);
+        }
+
+        if(array_key_exists('department', $attributes))
+        {
+            $this->setAttribute('department', $attributes['department'][0]);
+        }
+    }
+
+    /**
+     * Applies the entry's computer attributes.
      *
      * @param array $attributes
      */

47616c0089ab05b1f1ea8c1e57b2394ca69ee713
Steve Bauman <steven_bauman@outlook.com>
2015-04-20 11:11:34 -0400

    Added more search tests

--- tests/AdldapSearchTest.php
+++ tests/AdldapSearchTest.php
@@ -12,7 +12,7 @@ class AdldapSearchTest extends AdldapBaseTest
         return new Adldap(array(), $connection, false);
     }
 
-    public function testGetQuery()
+    public function testSearchGetQuery()
     {
         $connection = $this->newConnectionMock();
 
@@ -25,8 +25,93 @@ class AdldapSearchTest extends AdldapBaseTest
             ->shouldReceive('close')->once()->andReturn(true);
 
         $query = $search->where('cn', '*')->getQuery();
-        
+
         $this->assertEquals('(cn=*)', $query);
+    }
+
+    public function testSearchGetWheres()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection
+            ->shouldReceive('escape')->once()->andReturn('value')
+            ->shouldReceive('close')->once()->andReturn(true);
+
+        $search->where('field', '=', 'value');
+
+        $expected = array(
+            array(
+                'field' => 'field',
+                'operator' => '=',
+                'value' => 'value',
+            )
+        );
+
+        $this->assertEquals($expected, $search->getWheres());
+    }
+
+    public function testSearchGetOrWheres()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection
+            ->shouldReceive('escape')->once()->andReturn('value')
+            ->shouldReceive('close')->once()->andReturn(true);
+
+        $search->orWhere('field', '=', 'value');
+
+        $expected = array(
+            array(
+                'field' => 'field',
+                'operator' => '=',
+                'value' => 'value',
+            )
+        );
+
+        $this->assertEquals($expected, $search->getOrWheres());
+    }
+
+    public function testSearchGetSelects()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection->shouldReceive('close')->once()->andReturn(true);
+
+        $selects = array(
+            'cn',
+            'dn'
+        );
+
+        $search->select($selects);
+
+        $this->assertEquals($selects, $search->getSelects());
+    }
+
+    public function testSearchHasSelects()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection->shouldReceive('close')->once()->andReturn(true);
+
+        $search->select('cn');
+
+        $this->assertTrue($search->hasSelects());
     }
 
     public function testSearchAll()
@@ -59,5 +144,69 @@ class AdldapSearchTest extends AdldapBaseTest
         );
 
         $this->assertEquals($expected, $search->all());
+    }
+
+    public function testSearchWhere()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $ldapResults = array(
+            'count' => 1,
+            array(
+                'cn' => array(
+                    'John Doe'
+                )
+            )
+        );
+
+        $connection
+            ->shouldReceive('escape')->once()->andReturn('*')
+            ->shouldReceive('search')->once()->andReturn('resource')
+            ->shouldReceive('getEntries')->once()->andReturn($ldapResults)
+            ->shouldReceive('close')->once()->andReturn(true);
+
+        $expected = array(
+            0 => array(
+                'cn' => 'John Doe'
+            )
+        );
+
+        $searchResults = $search->where('cn', '=', 'John Doe')->get();
+
+        $this->assertEquals($expected, $searchResults);
+    }
+
+    public function testSearchWhereInvalidOperator()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection->shouldReceive('close')->once()->andReturn(true);
+
+        $this->setExpectedException('Adldap\Exceptions\AdldapException');
+
+        $search->where('field', 'invalid operator', 'value');
+    }
+
+    public function testSearchOrWhereInvalidOperator()
+    {
+        $connection = $this->newConnectionMock();
+
+        $ad = $this->newAdldap($connection);
+
+        $search = new AdldapSearch($ad);
+
+        $connection->shouldReceive('close')->once()->andReturn(true);
+
+        $this->setExpectedException('Adldap\Exceptions\AdldapException');
+
+        $search->orWhere('field', 'invalid operator', 'value');
     }
 }

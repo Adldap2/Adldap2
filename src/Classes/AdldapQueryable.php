4acf5e5ab3eb075ae1a181f89eb04d6572cc43c0
Steve Bauman <steven_bauman@outlook.com>
2015-04-24 14:47:46 -0400

    Added base queryable class to help extend the same methods

---
+++ src/Classes/AdldapQueryable.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Adldap\Classes;
+
+/**
+ * The AdldapQueryable class. This class provides
+ * some standard methods to queryable classes that
+ * extend this class.
+ *
+ * Class AdldapQueryable
+ * @package Adldap\Classes
+ */
+class AdldapQueryable extends AdldapBase
+{
+    /**
+     * The LDAP objects class name.
+     *
+     * @var string
+     */
+    public $objectClass = '';
+
+    /**
+     * Returns all entries with the current object class.
+     *
+     * @param array $fields
+     * @param bool $sorted
+     * @param string $sortBy
+     * @param string $sortByDirection
+     * @return array|bool
+     */
+    public function all($fields = [], $sorted = true, $sortBy = 'cn', $sortByDirection = 'asc')
+    {
+        $search = $this->adldap->search()
+            ->select($fields)
+            ->where('objectClass', '=', $this->objectClass);
+
+        if ($sorted) {
+            $search->sortBy($sortBy, $sortByDirection);
+        }
+
+        return $search->get();
+    }
+
+    /**
+     * Finds a single entry using the objects current class
+     * and the specified common name.
+     *
+     * @param string $name
+     * @param array $fields
+     * @return array|bool
+     */
+    public function find($name, $fields = [])
+    {
+        $results = $this->adldap->search()
+            ->select($fields)
+            ->where('objectClass', '=', $this->objectClass)
+            ->where('anr', '=', $name)
+            ->first();
+
+        if(count($results) > 0) {
+            return $results;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the DN of the current object class.
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function dn($name)
+    {
+        $info = $this->find($name);
+
+        if(is_array($info) && array_key_exists('dn', $info)) {
+            return $info['dn'];
+        }
+
+        return false;
+    }
+
+    /**
+     * Get information about a specific computer. Returned in a raw array format from AD.
+     *
+     * @param string $computerName The name of the computer
+     * @param array  $fields       Attributes to return
+     *
+     * @return array|bool
+     */
+    public function info($name, $fields = [])
+    {
+        return $this->find($name, $fields);
+    }
+}

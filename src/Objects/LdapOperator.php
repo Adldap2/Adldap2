2c2f532848a1f1716e9220c39c2d436418751f6b
Steve Bauman <steven_bauman@outlook.com>
2015-04-24 09:50:08 -0400

    Moved operators to separate class named LdapOperator

---
+++ src/Objects/LdapOperator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Adldap\Objects;
+
+/**
+ * Class Operator
+ * @package Adldap\Objects
+ */
+class LdapOperator extends AbstractObject
+{
+    /**
+     * The wildcard operator.
+     *
+     * @var string
+     */
+    public static $wildcard = '*';
+
+    /**
+     * The equals operator.
+     *
+     * @var string
+     */
+    public static $equals = '=';
+
+    /**
+     * The does not equal operator.
+     *
+     * @var string
+     */
+    public static $doesNotEqual = '!';
+
+    /**
+     * The greater than or equal to operator
+     *
+     * @var string
+     */
+    public static $greaterThanOrEqual = '>=';
+
+    /**
+     * The less than or equal to operator.
+     *
+     * @var string
+     */
+    public static $lessThanOrEqual = '<=';
+
+    /**
+     * The approximately equal to operator.
+     *
+     * @var string
+     */
+    public static $approximateEqual = '~=';
+
+    /**
+     * The and operator.
+     *
+     * @var string
+     */
+    public static $and = '&';
+
+    /**
+     * The or operator.
+     *
+     * @var string
+     */
+    public static $or = '|';
+}

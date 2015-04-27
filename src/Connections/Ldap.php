0175888e73668e459f77a5797340cc110413a3c2
Steve Bauman <steven_bauman@outlook.com>
2015-04-27 13:30:36 -0400

    Separated ldap function support checking into separate trait

--- src/Connections/Ldap.php
+++ src/Connections/Ldap.php
@@ -3,6 +3,7 @@
 namespace Adldap\Connections;
 
 use Adldap\Exceptions\AdldapException;
+use Adldap\Traits\LdapFunctionSupportTrait;
 use Adldap\Interfaces\ConnectionInterface;
 
 /**
@@ -12,6 +13,8 @@ use Adldap\Interfaces\ConnectionInterface;
  */
 class Ldap implements ConnectionInterface
 {
+    use LdapFunctionSupportTrait;
+
     /**
      * The SSL LDAP protocol string.
      *
@@ -80,52 +83,6 @@ class Ldap implements ConnectionInterface
      * @var bool
      */
     protected $suppressErrors = true;
-
-    /**
-     * Returns true / false if the current
-     * PHP install supports LDAP.
-     *
-     * @return bool
-     */
-    public function isSupported()
-    {
-        return function_exists('ldap_connect');
-    }
-
-    /**
-     * Returns true / false if the current
-     * PHP install supports an SASL bound
-     * LDAP connection.
-     *
-     * @return bool
-     */
-    public function isSaslSupported()
-    {
-        return function_exists('ldap_sasl_bind');
-    }
-
-    /**
-     * Returns true / false if the current
-     * PHP install supports LDAP paging.
-     *
-     * @return bool
-     */
-    public function isPagingSupported()
-    {
-        return function_exists('ldap_control_paged_result');
-    }
-
-    /**
-     * Returns true / false if the current if the current
-     * PHP install supports batch modification.
-     * Requires PHP 5.4 >= 5.4.26, PHP 5.5 >= 5.5.10 or PHP 5.6 >= 5.6.0.
-     *
-     * @return bool
-     */
-    public function isBatchSupported()
-    {
-        return function_exists('ldap_modify_batch');
-    }
 
     /**
      * Returns true / false if the

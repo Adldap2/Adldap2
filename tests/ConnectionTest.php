c4b17941d9cd40582b3161ab24c5f9f6447e5d19
Steve Bauman <steven_bauman@outlook.com>
2015-04-27 16:34:59 -0400

    Added manual escaping tests

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -44,4 +44,56 @@ class ConnectionTest extends FunctionalTestCase
 
         $this->assertTrue($passes);
     }
+
+    public function testEscapeManual()
+    {
+        $ldap = $this->mock('Adldap\Connections\Ldap')->makePartial();
+
+        $ldap->shouldAllowMockingProtectedMethods(true);
+
+        $expected = '\74\65\73\74\69\6e\67\3d\2b\3c\3e\22\22\3b\3a\23\28\29*\5c\78\30\30';
+
+        $result = $ldap->escapeManual('testing=+<>"";:#()*\x00', '*');
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testEscapeManualFilter()
+    {
+        $ldap = $this->mock('Adldap\Connections\Ldap')->makePartial();
+
+        $ldap->shouldAllowMockingProtectedMethods(true);
+
+        $expected = 'testing=+<>"";:#\28\29*\5cx00';
+
+        $result = $ldap->escapeManual('testing=+<>"";:#()*\x00', '*', 1);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testEscapeManualDn()
+    {
+        $ldap = $this->mock('Adldap\Connections\Ldap')->makePartial();
+
+        $ldap->shouldAllowMockingProtectedMethods(true);
+
+        $expected = 'testing\3d\2b\3c\3e\22\22\3b:\23()*\5cx00';
+
+        $result = $ldap->escapeManual('testing=+<>"";:#()*\x00', '*', 2);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testEscapeManualBothFilterAndDn()
+    {
+        $ldap = $this->mock('Adldap\Connections\Ldap')->makePartial();
+
+        $ldap->shouldAllowMockingProtectedMethods(true);
+
+        $expected = 'testing\3d\2b\3c\3e\22\22\3b:\23\5c28\5c29*\5c5cx00';
+
+        $result = $ldap->escapeManual('testing=+<>"";:#()*\x00', '*', 3);
+
+        $this->assertEquals($expected, $result);
+    }
 }

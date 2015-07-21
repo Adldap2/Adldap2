6bc94662ef2937bba892bb13fb5989273de08294
Steve Bauman <steven_bauman@outlook.com>
2015-07-21 15:04:58 -0400

    Remove unneeded method

--- src/Classes/Contacts.php
+++ src/Classes/Contacts.php
@@ -2,8 +2,6 @@
 
 namespace Adldap\Classes;
 
-use Adldap\Objects\Contact;
-
 class Contacts extends AbstractQueryable
 {
     /**
@@ -12,24 +10,4 @@ class Contacts extends AbstractQueryable
      * @var string
      */
     public $objectClass = 'contact';
-
-    /**
-     * Mail enable a contact. Allows email to be sent to them through Exchange.
-     *
-     * @param string $contactName  The contacts name
-     * @param string $emailAddress The contacts email address
-     * @param null   $mailNickname
-     *
-     * @return bool
-     */
-    public function contactMailEnable($contactName, $emailAddress, $mailNickname = null)
-    {
-        $contactDn = $this->dn($contactName);
-
-        if ($contactDn) {
-            return $this->adldap->exchange()->contactMailEnable($contactDn, $emailAddress, $mailNickname);
-        }
-
-        return false;
-    }
 }

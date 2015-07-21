e30a714f5929aae6de9735a369271bd86ddb7c15
Steve Bauman <steven_bauman@outlook.com>
2015-07-21 14:50:05 -0400

    Remove unused method

--- src/Classes/Containers.php
+++ src/Classes/Containers.php
@@ -55,61 +55,6 @@ class Containers extends AbstractQueryable
     }
 
     /**
-     * Returns a folder listing for a specific OU.
-     * See http://adldap.sourceforge.net/wiki/doku.php?id=api_folder_functions.
-     *
-     * If folderName is set to NULL this will list the root, strongly recommended
-     * to set $recursive to false in that instance!
-     *
-     * @param array  $folders
-     * @param string $dnType
-     * @param null   $recursive
-     * @param null   $type
-     *
-     * @return array|bool
-     */
-    public function listing($folders = [], $dnType = Adldap::ADLDAP_FOLDER, $recursive = null, $type = null)
-    {
-        $search = $this->adldap->search();
-
-        if (is_array($folders) && count($folders) > 0) {
-            /*
-             * Reverse the folder array so it's more
-             * akin to navigating a folder structure
-             */
-            $folders = array_reverse($folders);
-
-            /*
-             * Get the combined OU string for the search.
-             *
-             * ex. OU=Users,OU=Acme
-             */
-            $ou = $dnType.'='.implode(','.$dnType.'=', $folders);
-
-            $search->where('distinguishedname', '!', $ou.$this->adldap->getBaseDn());
-
-            // Apply the OU to the base DN
-            $dn = $ou.','.$this->adldap->getBaseDn();
-
-            $search->setDn($dn);
-        } else {
-            $search->where('distinguishedname', '!', $this->adldap->getBaseDn());
-        }
-
-        if ($type === null) {
-            $search->where('objectClass', '*');
-        } else {
-            $search->where('objectClass', '=', $type);
-        }
-
-        if ($recursive === false) {
-            $search->recursive(false);
-        }
-
-        return $search->get();
-    }
-
-    /**
      * Create an organizational unit.
      *
      * @param array $attributes Default attributes of the ou

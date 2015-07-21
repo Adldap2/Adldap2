f57995f21fce120153f6e5f46b6816fb09ac9677
Steve Bauman <steven_bauman@outlook.com>
2015-07-21 15:19:33 -0400

    Remove unused method

--- src/Classes/Computers.php
+++ src/Classes/Computers.php
@@ -10,37 +10,4 @@ class Computers extends AbstractQueryable
      * @var string
      */
     public $objectClass = 'computer';
-
-    /**
-     * Get the groups a computer is in.
-     *
-     * @param string $computerName The name of the computer
-     * @param null   $recursive    Whether to check recursively
-     *
-     * @return array|bool
-     */
-    public function groups($computerName, $recursive = null)
-    {
-        if ($recursive === null) {
-            $recursive = $this->adldap->getRecursiveGroups();
-        }
-
-        $info = $this->find($computerName);
-
-        if (is_array($info) && array_key_exists('memberof', $info)) {
-            $groups = $this->adldap->utilities()->niceNames($info['memberof']);
-
-            if ($recursive === true) {
-                foreach ($groups as $id => $groupName) {
-                    $extraGroups = $this->adldap->group()->recursiveGroups($groupName);
-
-                    $groups = array_merge($groups, $extraGroups);
-                }
-            }
-
-            return $groups;
-        }
-
-        return false;
-    }
 }

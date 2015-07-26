2fdd2a4d7b1194b0cef25e59cd3a21232dc958c9
Steve Bauman <steven_bauman@outlook.com>
2015-07-25 22:04:21 -0400

    Added the ability to remove ous, cns and dcs

--- src/Objects/DistinguishedName.php
+++ src/Objects/DistinguishedName.php
@@ -40,7 +40,7 @@ class DistinguishedName
 
         $dcs = $this->assembleRdns(ActiveDirectory::DOMAIN_COMPONENT, $this->domainComponents);
 
-        return implode(',', [$cns, $ous, $dcs]);
+        return implode(',', array_filter([$cns, $ous, $dcs]));
     }
 
     /**
@@ -53,6 +53,20 @@ class DistinguishedName
     public function addDc($dc)
     {
         $this->domainComponents[] = $dc;
+
+        return $this;
+    }
+
+    /**
+     * Removes a DC from the DC array.
+     *
+     * @param string $dc
+     *
+     * @return $this
+     */
+    public function removeDc($dc)
+    {
+        $this->domainComponents = array_diff($this->domainComponents, [$dc]);
 
         return $this;
     }
@@ -72,6 +86,20 @@ class DistinguishedName
     }
 
     /**
+     * Removes a CN from the CN array.
+     *
+     * @param string $cn
+     *
+     * @return $this
+     */
+    public function removeCn($cn)
+    {
+        $this->commonNames = array_diff($this->commonNames, [$cn]);
+
+        return $this;
+    }
+
+    /**
      * Adds an OU to the OU array.
      *
      * @param string $ou
@@ -86,19 +114,37 @@ class DistinguishedName
     }
 
     /**
+     * Removes an OU from the OU array.
+     *
+     * @param string $ou
+     *
+     * @return $this
+     */
+    public function removeOu($ou)
+    {
+        $this->organizationUnits = array_diff($this->organizationUnits, [$ou]);
+
+        return $this;
+    }
+
+    /**
      * Assembles an RDN with the specified attribute and value.
      *
      * @param string $attribute
      * @param array  $values
      *
-     * @return string
+     * @return null|string
      */
     private function assembleRdns($attribute, array $values = [])
     {
-        $values = array_map(function($value) use ($attribute) {
-            return $attribute.'='.$value;
-        }, $values);
+        if(count($values) > 0) {
+            $values = array_map(function($value) use ($attribute) {
+                return $attribute.'='.$value;
+            }, $values);
 
-        return implode(',', $values);
+            return implode(',', $values);
+        }
+
+        return null;
     }
 }

da367e47634cb2d59855c5b0583981e22c7b6041
Steve Bauman <steven_bauman@outlook.com>
2015-07-28 17:35:59 -0400

    Added create and newInstance methods for contacts

--- src/Classes/Contacts.php
+++ src/Classes/Contacts.php
@@ -2,6 +2,9 @@
 
 namespace Adldap\Classes;
 
+use Adldap\Models\User;
+use Adldap\Schemas\ActiveDirectory;
+
 class Contacts extends AbstractQueryable
 {
     /**
@@ -10,4 +13,34 @@ class Contacts extends AbstractQueryable
      * @var string
      */
     public $objectClass = 'contact';
+
+    /**
+     * Returns a new User instance.
+     *
+     * @param array $attributes
+     *
+     * @return User
+     */
+    public function newInstance(array $attributes = [])
+    {
+        return (new User($attributes, $this->connection))
+            ->setAttribute(ActiveDirectory::OBJECT_CLASS, [
+                ActiveDirectory::TOP,
+                ActiveDirectory::PERSON,
+                ActiveDirectory::ORGANIZATIONAL_PERSON,
+                ActiveDirectory::CONTACT,
+            ]);
+    }
+
+    /**
+     * Creates a new contact and returns the result.
+     *
+     * @param array $attributes
+     *
+     * @return bool
+     */
+    public function create(array $attributes = [])
+    {
+        return $this->newInstance($attributes)->save();
+    }
 }

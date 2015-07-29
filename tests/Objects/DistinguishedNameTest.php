fcfea9cd6da7b4405741ec02b724b0b88c1815ab
Steve Bauman <steven_bauman@outlook.com>
2015-07-29 11:49:45 -0400

    Added more DN object tests

--- tests/Objects/DistinguishedNameTest.php
+++ tests/Objects/DistinguishedNameTest.php
@@ -36,4 +36,68 @@ class DistinguishedNameTest extends UnitTestCase
 
         $this->assertEquals($base->get(), $dn->get());
     }
+
+    public function testAddDc()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addDc('org');
+        $dn->addDc('corp');
+
+        $this->assertEquals('dc=corp,dc=org', $dn->get());
+    }
+
+    public function testRemoveDc()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addDc('org');
+        $dn->addDc('corp');
+
+        $dn->removeDc('org');
+
+        $this->assertEquals('dc=corp', $dn->get());
+    }
+
+    public function testAddOu()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addOu('User Accounts');
+        $dn->addOu('Accounting');
+
+        $this->assertEquals('ou=Accounting,ou=User Accounts', $dn->get());
+    }
+
+    public function testRemoveOu()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addOu('User Accounts');
+        $dn->addOu('Accounting');
+
+        $dn->removeOu('User Accounts');
+
+        $this->assertEquals('ou=Accounting', $dn->get());
+    }
+
+    public function testAddCn()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addCn('Testing');
+
+        $this->assertEquals('cn=Testing', $dn->get());
+    }
+
+    public function testRemoveCn()
+    {
+        $dn = new DistinguishedName();
+
+        $dn->addCn('Testing');
+
+        $dn->removeCn('Testing');
+
+        $this->assertEmpty($dn->get());
+    }
 }

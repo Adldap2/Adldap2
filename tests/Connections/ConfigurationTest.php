8495be0479b620bf12265d938a28497d17b577ae
Steve Bauman <steven_bauman@outlook.com>
2015-07-29 10:10:17 -0400

    More tests

--- tests/Connections/ConfigurationTest.php
+++ tests/Connections/ConfigurationTest.php
@@ -17,9 +17,54 @@ class ConfigurationTest extends UnitTestCase
         $this->assertNull($config->getAdminUsername());
         $this->assertNull($config->getAdminPassword());
         $this->assertNull($config->getBaseDn());
-        $this->assertEquals([], $config->getPersonFilter());
         $this->assertFalse($config->getUseSSO());
         $this->assertFalse($config->getUseSSL());
         $this->assertFalse($config->getUseTLS());
+    }
+
+    public function testSetPort()
+    {
+        $config = new Configuration();
+
+        $config->setPort(500);
+
+        $this->assertEquals('500', $config->getPort());
+        $this->assertInternalType('string', $config->getPort());
+    }
+
+    public function testSetDomainControllers()
+    {
+        $config = new Configuration();
+
+        $controllers = ['dc1', 'dc2'];
+
+        $config->setDomainControllers($controllers);
+
+        $this->assertEquals($controllers, $config->getDomainControllers());
+    }
+
+    public function testSetDomainControllersInvalidType()
+    {
+        $config = new Configuration();
+
+        try {
+            $config->setDomainControllers('Invalid Type');
+
+            $passes = false;
+        } catch (\Exception $e) {
+            $passes = true;
+        }
+
+        $this->assertTrue($passes);
+    }
+
+    public function testSetFollowReferrals()
+    {
+        $config = new Configuration();
+
+        $config->setFollowReferrals('1');
+
+        $this->assertEquals(1, $config->getFollowReferrals());
+        $this->assertInternalType('int', $config->getFollowReferrals());
     }
 }

d3ba355ffd3d975483ab0b76f12fe4b43fdd973a
Steve Bauman <steven_bauman@outlook.com>
2015-07-30 15:13:23 -0400

    Added ldap connection tests

---
+++ tests/Connections/LdapTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Adldap\Tests\Connections;
+
+use Adldap\Connections\Ldap;
+use Adldap\Tests\UnitTestCase;
+
+class LdapTest extends UnitTestCase
+{
+    public function testConstructDefaults()
+    {
+        $ldap = new Ldap();
+
+        $this->assertFalse($ldap->isUsingTLS());
+        $this->assertFalse($ldap->isUsingSSL());
+        $this->assertFalse($ldap->isUsingSSO());
+        $this->assertFalse($ldap->isBound());
+        $this->assertTrue($ldap->isSupported());
+        $this->assertNull($ldap->getConnection());
+    }
+
+    public function testSuppressErrors()
+    {
+        $ldap = new Ldap();
+
+        $ldap->suppressErrors();
+
+        $this->assertNull($ldap->add('', []));
+
+        $ldap->showErrors();
+
+        try {
+            $ldap->add('', []);
+
+            $passes = false;
+        } catch (\Exception $e) {
+            $passes = true;
+        }
+
+        $this->assertTrue($passes);
+    }
+}

519ebc8206c0b395fdc4430be703b9b3bb974981
Steve Bauman <steven_bauman@outlook.com>
2015-07-31 16:18:38 -0400

    Move all standard attribute methods into AbstractModel
    - Moved all standard AD attribute methods into AbstractModel
    - getObjectCategory now returns the model it's associated to

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -5,6 +5,7 @@ namespace Adldap\Models;
 use Adldap\Exceptions\AdldapException;
 use Adldap\Exceptions\ModelNotFoundException;
 use Adldap\Schemas\ActiveDirectory;
+use Adldap\Classes\Utilities;
 use Adldap\Adldap;
 
 abstract class AbstractModel
@@ -85,7 +86,217 @@ abstract class AbstractModel
     }
 
     /**
-     * Returns the entry's distinguished name string.
+     * Returns the model's name. An AD alias for the CN attribute.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms675449(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->getAttribute(ActiveDirectory::NAME, 0);
+    }
+
+    /**
+     * Sets the model's name.
+     *
+     * @param string $name
+     *
+     * @return Entry
+     */
+    public function setName($name)
+    {
+        return $this->setAttribute(ActiveDirectory::NAME, $name, 0);
+    }
+
+    /**
+     * Returns the model's common name.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms675449(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getCommonName()
+    {
+        return $this->getAttribute(ActiveDirectory::COMMON_NAME, 0);
+    }
+
+    /**
+     * Sets the model's common name.
+     *
+     * @param string $name
+     *
+     * @return Entry
+     */
+    public function setCommonName($name)
+    {
+        return $this->setAttribute(ActiveDirectory::COMMON_NAME, $name, 0);
+    }
+
+    /**
+     * Returns the model's samaccountname.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms679635(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getAccountName()
+    {
+        return $this->getAttribute(ActiveDirectory::ACCOUNT_NAME, 0);
+    }
+
+    /**
+     * Returns the model's samaccounttype.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms679637(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getAccountType()
+    {
+        return $this->getAttribute(ActiveDirectory::ACCOUNT_TYPE, 0);
+    }
+
+    /**
+     * Returns the model's `when created` time.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms680924(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->getAttribute(ActiveDirectory::CREATED_AT, 0);
+    }
+
+    /**
+     * Returns the model's `when changed` time.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms680921(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getUpdatedAt()
+    {
+        return $this->getAttribute(ActiveDirectory::UPDATED_AT, 0);
+    }
+
+    /**
+     * Returns the Container of the current Model.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms679012(v=vs.85).aspx
+     *
+     * @return Container|Entry|bool
+     */
+    public function getObjectClass()
+    {
+        return $this->getAdldap()->search()->findByDn($this->getObjectCategoryDn());
+    }
+
+    /**
+     * Returns the CN of the model's object category.
+     *
+     * @return null|string
+     */
+    public function getObjectCategory()
+    {
+        $category = $this->getObjectCategoryArray();
+
+        if(is_array($category) && array_key_exists(0, $category)) {
+            return $category[0];
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the model's object category DN in an exploded array.
+     *
+     * @return array
+     */
+    public function getObjectCategoryArray()
+    {
+        return Utilities::explodeDn($this->getObjectCategoryDn());
+    }
+
+    /**
+     * Returns the model's object category DN string.
+     *
+     * @return array
+     */
+    public function getObjectCategoryDn()
+    {
+        return $this->getAttribute(ActiveDirectory::OBJECT_CATEGORY, 0);
+    }
+
+    /**
+     * Returns the model's object SID.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms679024(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getObjectSid()
+    {
+        return $this->getAttribute(ActiveDirectory::OBJECT_SID, 0);
+    }
+
+    /**
+     * Returns the model's primary group ID.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms679375(v=vs.85).aspx
+     *
+     * @return string
+     */
+    public function getPrimaryGroupId()
+    {
+        return $this->getAttribute(ActiveDirectory::PRIMARY_GROUP_ID, 0);
+    }
+
+    /**
+     * Returns the model's instance type.
+     *
+     * https://msdn.microsoft.com/en-us/library/ms676204(v=vs.85).aspx
+     *
+     * @return int
+     */
+    public function getInstanceType()
+    {
+        return $this->getAttribute(ActiveDirectory::INSTANCE_TYPE, 0);
+    }
+
+    /**
+     * Returns the model's GUID.
+     *
+     * @return string
+     */
+    public function getGuid()
+    {
+        return $this->getAttribute(ActiveDirectory::OBJECT_GUID, 0);
+    }
+
+    /**
+     * Returns the model's SID.
+     *
+     * @return string
+     */
+    public function getSid()
+    {
+        return $this->getAttribute(ActiveDirectory::OBJECT_SID, 0);
+    }
+
+    /**
+     * Returns the model's max password age.
+     *
+     * @return string
+     */
+    public function getMaxPasswordAge()
+    {
+        return $this->getAttribute(ActiveDirectory::MAX_PASSWORD_AGE, 0);
+    }
+
+    /**
+     * Returns the model's distinguished name string.
      *
      * https://msdn.microsoft.com/en-us/library/aa366101(v=vs.85).aspx
      *
@@ -97,7 +308,7 @@ abstract class AbstractModel
     }
 
     /**
-     * Sets the entry's distinguished name attribute.
+     * Sets the model's distinguished name attribute.
      *
      * @param string $dn
      *
@@ -109,7 +320,7 @@ abstract class AbstractModel
     }
 
     /**
-     * Returns the entry's distinguished name string.
+     * Returns the model's distinguished name string.
      *
      * (Alias for getDistinguishedName())
      *
@@ -123,7 +334,7 @@ abstract class AbstractModel
     }
 
     /**
-     *  Sets the entry's distinguished name attribute.
+     *  Sets the model's distinguished name attribute.
      *
      * (Alias for setDistinguishedName())
      *
@@ -138,7 +349,7 @@ abstract class AbstractModel
 
     /**
      * Syncs the original attributes with
-     * the entry's current attributes.
+     * the model's current attributes.
      *
      * @return $this
      */

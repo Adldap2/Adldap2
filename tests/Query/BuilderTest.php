17ded470ebc670c30d7917ec134233b01ca9138c
Steve Bauman <steven_bauman@outlook.com>
2015-08-28 09:37:25 -0400

    Added more builder tests

--- tests/Query/BuilderTest.php
+++ tests/Query/BuilderTest.php
@@ -222,4 +222,142 @@ class BuilderTest extends UnitTestCase
 
         $b->orWhere('field', 'invalid', 'value');
     }
+
+    public function testBuiltWhere()
+    {
+        $b = new Builder();
+
+        $b->where('field', '=' , 'value');
+
+        $expected = '(field=\76\61\6c\75\65)';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltWheres()
+    {
+        $b = new Builder();
+
+        $b->where('field', '=' , 'value');
+
+        $b->where('other', '=', 'value');
+
+        $expected = '(&(field=\76\61\6c\75\65)(other=\76\61\6c\75\65))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltWhereStartsWith()
+    {
+        $b = new Builder();
+
+        $b->whereStartsWith('field', 'value');
+
+        $expected = '(field=\76\61\6c\75\65*)';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltWhereEndsWith()
+    {
+        $b = new Builder();
+
+        $b->whereEndsWith('field', 'value');
+
+        $expected = '(field=*\76\61\6c\75\65)';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltWhereContains()
+    {
+        $b = new Builder();
+
+        $b->whereContains('field', 'value');
+
+        $expected = '(field=*\76\61\6c\75\65*)';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltOrWhere()
+    {
+        $b = new Builder();
+
+        $b->orWhere('field', '=' , 'value');
+
+        $expected = '(&(|(field=\76\61\6c\75\65)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltOrWheres()
+    {
+        $b = new Builder();
+
+        $b->orWhere('field', '=' , 'value');
+
+        $b->orWhere('other', '=', 'value');
+
+        $expected = '(&(|(field=\76\61\6c\75\65)(other=\76\61\6c\75\65)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltOrWhereStartsWith()
+    {
+        $b = new Builder();
+
+        $b->orWhereStartsWith('field', 'value');
+
+        $expected = '(&(|(field=\76\61\6c\75\65*)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltOrWhereEndsWith()
+    {
+        $b = new Builder();
+
+        $b->orWhereEndsWith('field', 'value');
+
+        $expected = '(&(|(field=*\76\61\6c\75\65)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltOrWhereContains()
+    {
+        $b = new Builder();
+
+        $b->orWhereContains('field', 'value');
+
+        $expected = '(&(|(field=*\76\61\6c\75\65*)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testBuiltWhereAndOrWheres()
+    {
+        $b = new Builder();
+
+        $b->where('field', '=', 'value');
+
+        $b->orWhere('or', '=', 'value');
+
+        $expected = '(&(field=\76\61\6c\75\65)(|(or=\76\61\6c\75\65)))';
+
+        $this->assertEquals($expected, $b->get());
+    }
+
+    public function testWrap()
+    {
+        $b = new Builder();
+
+        $wrapped = $b->wrap('test');
+
+        $expected = '(test)';
+
+        $this->assertEquals($expected, $wrapped);
+    }
 }

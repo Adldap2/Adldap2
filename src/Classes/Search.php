565e81e2b528fd96bd681404b70e8a897393ca32
Steve Bauman <steven_bauman@outlook.com>
2015-09-09 11:01:06 -0400

    Fix __call magic method parameters

--- src/Classes/Search.php
+++ src/Classes/Search.php
@@ -84,7 +84,7 @@ class Search extends AbstractBase
      *
      * @return mixed
      */
-    public function __call($method, array $parameters = [])
+    public function __call($method, $parameters)
     {
         return call_user_func_array([$this->query, $method], $parameters);
     }

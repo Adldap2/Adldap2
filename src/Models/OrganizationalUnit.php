02fa3ed4b258140a2dfdbe843997e71b0bae96d2
Steve Bauman <steven_bauman@outlook.com>
2015-09-24 10:10:44 -0400

    Added getOu method to OU model

--- src/Models/OrganizationalUnit.php
+++ src/Models/OrganizationalUnit.php
@@ -4,5 +4,13 @@ namespace Adldap\Models;
 
 class OrganizationalUnit extends AbstractModel
 {
-    //
+    /**
+     * Retrieves the organization units OU attribute.
+     *
+     * @return string
+     */
+    public function getOu()
+    {
+        return $this->getAttribute('ou', 0);
+    }
 }

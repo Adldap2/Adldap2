53846eda0669b2297b3105f8e45e0eb73b5b390d
Steve Bauman <steven_bauman@outlook.com>
2015-10-21 12:11:25 -0400

    Working on BatchModification tests

---
+++ tests/Objects/BatchModificationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Adldap\tests\Objects;
+
+use Adldap\Objects\BatchModification;
+use Adldap\Tests\UnitTestCase;
+
+class BatchModificationTest extends UnitTestCase
+{
+    public function testBuildWithOriginal()
+    {
+        $modification = new BatchModification();
+
+        $modification->setOriginal('Test');
+        $modification->setAttribute('cn');
+        $modification->setValues(['New CN']);
+
+        $modification->build();
+
+        $this->assertEquals(LDAP_MODIFY_BATCH_REPLACE, $modification->getType());
+    }
+
+    public function testBuildWithoutOriginal()
+    {
+        $modification = new BatchModification();
+
+        $modification->setAttribute('cn');
+        $modification->setValues(['New CN']);
+
+        $modification->build();
+
+        $this->assertEquals(LDAP_MODIFY_BATCH_ADD, $modification->getType());
+    }
+
+    public function testBuildWithOriginalAndNullValue()
+    {
+        $modification = new BatchModification();
+
+        $modification->setOriginal('Test');
+        $modification->setAttribute('cn');
+        $modification->setValues([null]);
+
+        $modification->build();
+
+        $this->assertEquals(LDAP_MODIFY_BATCH_REMOVE_ALL, $modification->getType());
+    }
+}

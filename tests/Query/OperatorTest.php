fa92413a47ee9bff20f7141db3b51c36df058426
Steve Bauman <steven_bauman@outlook.com>
2015-10-29 15:03:59 -0400

    Added operator test

---
+++ tests/Query/OperatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Adldap\Tests\Query;
+
+use Adldap\Query\Operator;
+use Adldap\Tests\UnitTestCase;
+
+class OperatorTest extends UnitTestCase
+{
+    public function testAll()
+    {
+        $operators = Operator::all();
+
+        $expected = [
+            'has'                   => '*',
+            'notHas'                => '!*',
+            'equals'                => '=',
+            'doesNotEqual'          => '!',
+            'greaterThanOrEquals'   => '>=',
+            'lessThanOrEquals'      => '<=',
+            'approximatelyEquals'   => '~=',
+            'startsWith'            => 'starts_with',
+            'notStartsWith'         => 'not_starts_with',
+            'endsWith'              => 'ends_with',
+            'notEndsWith'           => 'not_ends_with',
+            'contains'              => 'contains',
+            'notContains'           => 'not_contains',
+        ];
+
+        $this->assertEquals($expected, $operators);
+    }
+}

291c6c8f758f28a89aa227f5ff4c238bbf4480fd
Steve Bauman <steven_bauman@outlook.com>
2016-02-10 11:57:59 -0500

    Added manager test

---
+++ tests/Connections/ManagerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Adldap\Tests\Connections;
+
+use Adldap\Exceptions\AdldapException;
+use Adldap\Connections\Provider;
+use Adldap\Connections\Manager;
+use Adldap\Tests\UnitTestCase;
+
+class ManagerTest extends UnitTestCase
+{
+    public function test_connection_does_not_exist()
+    {
+        $manager = new Manager();
+
+        $this->setExpectedException(AdldapException::class);
+
+        $manager->get('missing');
+    }
+
+    public function test_remove()
+    {
+        $provider = $this->mock(Provider::class);
+
+        $manager = new Manager();
+
+        $manager->add('testing', $provider);
+
+        $this->assertInstanceOf(get_class($provider), $manager->get('testing'));
+
+        $manager->remove('testing');
+
+        $this->setExpectedException(AdldapException::class);
+
+        $manager->get('testing');
+    }
+}

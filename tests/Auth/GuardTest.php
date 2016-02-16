9f74549aa03374ba7aca8e46af277b21b42e396f
Steve Bauman <steven_bauman@outlook.com>
2016-02-16 14:39:33 -0500

    Added auth guard tests

---
+++ tests/Auth/GuardTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Adldap\Tests\Auth;
+
+use Adldap\Auth\Guard;
+use Adldap\Connections\Configuration;
+use Adldap\Connections\Ldap;
+use Adldap\Tests\UnitTestCase;
+
+class GuardTest extends UnitTestCase
+{
+    public function test_validate_username()
+    {
+        $guard = new Guard(new Ldap(), new Configuration());
+
+        $this->setExpectedException('Adldap\Exceptions\Auth\UsernameRequiredException');
+
+        $guard->attempt('', 'password');
+    }
+
+    public function test_validate_password()
+    {
+        $guard = new Guard(new Ldap(), new Configuration());
+
+        $this->setExpectedException('Adldap\Exceptions\Auth\PasswordRequiredException');
+
+        $guard->attempt('username', '');
+    }
+
+    public function test_attempt()
+    {
+        $config = $this->mock(Configuration::class);
+
+        $config->shouldReceive('getAccountSuffix')->once()->andReturn('@account-suffix')
+            ->shouldReceive('getAdminUsername')->once()->andReturn('admin')
+            ->shouldReceive('getAdminPassword')->once()->andReturn('password')
+            ->shouldReceive('getAdminAccountSuffix')->once()->andReturn('@admin-suffix');
+
+        $ldap = $this->mock(Ldap::class);
+
+        $ldap->shouldReceive('bind')->twice()->andReturn(true);
+
+        $guard = new Guard($ldap, $config);
+
+        $this->assertTrue($guard->attempt('username', 'password'));
+    }
+
+    public function test_bind_using_credentials()
+    {
+        $config = $this->mock(Configuration::class);
+
+        $config->shouldReceive('getAccountSuffix')->once()->andReturn('@account-suffix');
+
+        $ldap = $this->mock(Ldap::class);
+
+        $ldap->shouldReceive('bind')->once()->withArgs(['username@account-suffix', 'password'])->andReturn(true);
+
+        $guard = new Guard($ldap, $config);
+
+        $this->assertNull($guard->bindUsingCredentials('username', 'password'));
+    }
+
+    public function test_bind_as_administrator()
+    {
+        $config = $this->mock(Configuration::class);
+
+        $config->shouldReceive('getAdminUsername')->once()->andReturn('admin')
+            ->shouldReceive('getAdminPassword')->once()->andReturn('password')
+            ->shouldReceive('getAdminAccountSuffix')->once()->andReturn('@admin-suffix');
+
+        $ldap = $this->mock(Ldap::class);
+
+        $ldap->shouldReceive('bind')->once()->withArgs(['admin@admin-suffix', 'password'])->andReturn(true);
+
+        $guard = new Guard($ldap, $config);
+
+        $this->assertNull($guard->bindAsAdministrator());
+    }
+
+    public function test_bind_as_administrator_without_suffix()
+    {
+        $config = $this->mock(Configuration::class);
+
+        $config->shouldReceive('getAdminUsername')->once()->andReturn('admin')
+            ->shouldReceive('getAdminPassword')->once()->andReturn('password')
+            ->shouldReceive('getAdminAccountSuffix')->once()->andReturn(null)
+            ->shouldReceive('getAccountSuffix')->once()->andReturn('@account-suffix');
+
+        $ldap = $this->mock(Ldap::class);
+
+        $ldap->shouldReceive('bind')->once()->withArgs(['admin@account-suffix', 'password'])->andReturn(true);
+
+        $guard = new Guard($ldap, $config);
+
+        $this->assertNull($guard->bindAsAdministrator());
+    }
+}

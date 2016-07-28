3408033108c41919edb170579e387974f2368511
Steve Bauman <steven_bauman@outlook.com>
2016-07-28 10:01:36 -0400

    Working on more useful exceptions.

--- src/Exceptions/ModelDoesNotExistException.php
+++ src/Exceptions/ModelDoesNotExistException.php
@@ -4,5 +4,26 @@ namespace Adldap\Exceptions;
 
 class ModelDoesNotExistException extends AdldapException
 {
-    //
+    /**
+     * The class name of the model that does not exist.
+     *
+     * @var string
+     */
+    protected $model;
+
+    /**
+     * Sets the model that does not exist.
+     *
+     * @param string $model
+     *
+     * @return ModelDoesNotExistException
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        $this->message = "Model [{$model}] does not exist.";
+
+        return $this;
+    }
 }

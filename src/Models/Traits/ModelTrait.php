681047c7918e28f231a07899a2e13e20c3874204
Steve Bauman <steven_bauman@outlook.com>
2016-08-04 11:46:12 -0400

    Revert "Fix model trait code docs."
    This reverts commit 2b96fb82406073ab0253c827a387e2e0ea7b81b0.

--- src/Models/Traits/ModelTrait.php
+++ src/Models/Traits/ModelTrait.php
@@ -5,60 +5,32 @@ namespace Adldap\Models\Traits;
 trait ModelTrait
 {
     /**
-     * Returns the current query builder.
-     *
-     * @return \Adldap\Query\Builder
+     * {@inheritdoc}
      */
     abstract public function getQuery();
 
     /**
-     * Returns the current models schema.
-     *
-     * @return \Adldap\Contracts\Schemas\SchemaInterface
+     * {@inheritdoc}
      */
     abstract public function getSchema();
 
     /**
-     * Returns the model's distinguished name string.
-     *
-     * (Alias for getDistinguishedName())
-     *
-     * https://msdn.microsoft.com/en-us/library/aa366101(v=vs.85).aspx
-     *
-     * @return string|null
+     * {@inheritdoc}
      */
     abstract public function getDn();
 
     /**
-     * Returns the models attribute with the specified key.
-     *
-     * If a sub-key is specified, it will try and
-     * retrieve it from the parent keys array.
-     *
-     * @param int|string $key
-     * @param int|string $subKey
-     *
-     * @return mixed
+     * {@inheritdoc}
      */
     abstract public function getAttribute($key, $subKey = null);
 
     /**
-     * Sets attributes on the current entry.
-     *
-     * @param int|string $key
-     * @param mixed      $value
-     * @param int|string $subKey
-     *
-     * @return \Adldap\Models\Model
+     * {@inheritdoc}
      */
     abstract public function setAttribute($key, $value, $subKey = null);
 
     /**
-     * Converts the inserted string boolean to a PHP boolean.
-     *
-     * @param string $bool
-     *
-     * @return null|bool
+     * {@inheritdoc}
      */
     abstract public function convertStringToBool($bool);
 }

b23aaa430fa6ce5161252da8638f336d460c93b5
Steve Bauman <steven_bauman@outlook.com>
2016-08-17 16:31:02 -0400

    Added more Group tests

--- tests/Models/GroupTest.php
+++ tests/Models/GroupTest.php
@@ -3,13 +3,57 @@
 namespace Adldap\tests\Models;
 
 use Adldap\Models\Group;
+use Adldap\Query\Builder;
 use Adldap\Tests\TestCase;
+use Adldap\Schemas\ActiveDirectory;
 
 class GroupTest extends TestCase
 {
     protected function newGroupModel($attributes, $builder, $schema = null)
     {
         return new Group($attributes, $builder, $schema);
+    }
+
+    public function test_get_members()
+    {
+        $expected = $this->newGroupModel([
+            'cn' => ['test1'],
+            'dn' => 'cn=test1,dc=corp,dc=org'
+        ], $this->newBuilder());
+
+        $builder = $this->mock(Builder::class);
+
+        $builder
+            ->shouldReceive('getSchema')->once()->andReturn(new ActiveDirectory())
+            ->shouldReceive('newInstance')->once()->andReturn($builder)
+            ->shouldReceive('findByDn')->once()->andReturn($expected);
+
+        $group = $this->newGroupModel([
+            'member' => ['cn=test1,dc=corp,dc=org'],
+        ], $builder);
+
+        $members = $group->getMembers();
+
+        $this->assertCount(1, $members);
+        $this->assertEquals($expected->getCommonName(), $members->first()->getCommonName());
+        $this->assertEquals($expected->getDn(), $members->first()->getDn());
+    }
+
+    public function test_get_member_names()
+    {
+        $group = $this->newGroupModel([
+            'member' => [
+                'cn=test1,dc=corp,dc=org',
+                'cn=test2,dc=corp,dc=org',
+                'cn=test3,dc=corp,dc=org',
+            ]
+        ], $this->newBuilder());
+
+        $this->assertEquals([
+            'test1',
+            'test2',
+            'test3',
+        ], $group->getMemberNames());
     }
 
     public function test_in_group()

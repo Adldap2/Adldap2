6e5bcdc5772807b77fe1a6756b8117462bc293e3
Steve Bauman <steven_bauman@outlook.com>
2016-10-24 09:56:29 -0400

    Added unique OpenLDAP attribute

--- src/Schemas/OpenLDAP.php
+++ src/Schemas/OpenLDAP.php
@@ -23,6 +23,14 @@ class OpenLDAP extends ActiveDirectory
     /**
      * {@inheritdoc}
      */
+    public function objectGuid()
+    {
+        return 'entryuuid';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function distinguishedName()
     {
         return 'dn';

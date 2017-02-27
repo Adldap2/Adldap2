4b5f5e9789aeaab79d4d76916ba7881252565b90
Steve Bauman <steven_bauman@outlook.com>
2017-02-27 09:10:51 -0500

    Added comments for domain configuration options.

--- src/Configuration/DomainConfiguration.php
+++ src/Configuration/DomainConfiguration.php
@@ -18,20 +18,49 @@ class DomainConfiguration
      * @var array
      */
     protected $options = [
+        // An array of LDAP hosts.
         'domain_controllers' => [],
+
+        // The global LDAP operation timeout limit in seconds.
         'timeout' => 5,
+
+        // The LDAP version to utilize.
         'version' => 3,
+
+        // The port to use for connecting to your hosts.
         'port' => ConnectionInterface::PORT,
+
+        // The base distinguished name of your domain.
         'base_dn' => '',
+
+        // Whether or not to use SSL when connecting to your hosts.
         'use_ssl' => false,
+
+        // Whether or not to use TLS when connecting to your hosts.
         'use_tls' => false,
+
+        // Whether or not follow referrals is enabled when performing LDAP operations.
         'follow_referrals' => false,
+
+        // The account prefix to use when authenticating users.
         'account_prefix' => '',
+
+        // The account suffix to use when authenticating users.
         'account_suffix' => '',
+
+        // The username to connect to your hosts with.
         'admin_username' => '',
+
+        // The password that is utilized with the above user.
         'admin_password' => '',
+
+        // The account prefix to use when authenticating your admin account above.
         'admin_account_prefix' => '',
+
+        // The account prefix to use when authenticating your admin account above.
         'admin_account_suffix' => '',
+
+        // Custom LDAP options that you'd like to utilize.
         'custom_options' => [],
     ];
 

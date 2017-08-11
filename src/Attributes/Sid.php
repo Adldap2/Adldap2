d49a988b5f680718d3e117c82a54f730c2168683
Steve Bauman <steven_bauman@outlook.com>
2017-08-11 12:57:09 -0400

    Working on GUID / SID attributes

--- src/Attributes/Sid.php
+++ src/Attributes/Sid.php
@@ -2,6 +2,8 @@
 
 namespace Adldap\Attributes;
 
+use Adldap\Utilities;
+
 class Sid
 {
     /**
@@ -20,10 +22,7 @@ class Sid
      */
     public static function isValid($sid)
     {
-        return (bool) preg_match(
-            '/^S-\d(-\d{1,10}){1,16}$/i',
-            $sid
-        );
+        return Utilities::isValidSid($sid);
     }
 
     /**
@@ -37,7 +36,7 @@ class Sid
     {
         if (static::isValid($value)) {
             $this->value = $value;
-        } else if ($value = $this->convertFromBinary($value)) {
+        } else if ($value = $this->binarySidToString($value)) {
             $this->value = $value;
         } else {
             throw new \InvalidArgumentException("Invalid Binary / String SID.");
@@ -49,7 +48,7 @@ class Sid
      *
      * @return string
      */
-    public function getString()
+    public function getValue()
     {
         return $this->value;
     }
@@ -78,62 +77,14 @@ class Sid
     }
 
     /**
-     * Converts a string SID to binary.
+     * Returns the string variant of a binary SID.
      *
-     * @param $string
-     *
-     * @return string
-     */
-    public function convertFromString($string)
-    {
-        return pack('C2xxNV*', $string);
-    }
-
-    /**
-     * Convert a binary SID to a string SID.
-     *
-     * @author Chad Sikorra
-     *
-     * @link https://github.com/ChadSikorra
-     * @link https://stackoverflow.com/questions/39533560/php-ldap-get-user-sid
-     *
-     * @param string $binary The Binary SID
+     * @param string $binary
      *
      * @return string|null
      */
-    protected function convertFromBinary($binary)
+    protected function binarySidToString($binary)
     {
-        // Revision - 8bit unsigned int (C1)
-        // Count - 8bit unsigned int (C1)
-        // 2 null bytes
-        // ID - 32bit unsigned long, big-endian order
-        $sid = @unpack('C1rev/C1count/x2/N1id', $binary);
-
-        $subAuthorities = [];
-
-        if (!isset($sid['id']) || !isset($sid['rev'])) {
-            return;
-        }
-
-        $revisionLevel = $sid['rev'];
-
-        $identifierAuthority = $sid['id'];
-
-        $subs = isset($sid['count']) ? $sid['count'] : 0;
-
-        // The sub-authorities depend on the count, so only get as
-        // many as the count, regardless of data beyond it.
-        for ($i = 0; $i < $subs; $i++) {
-            // Each sub-auth is a 32bit unsigned long, little-endian order
-            $subAuthorities[] = unpack(
-                'V1sub',
-                hex2bin(substr(bin2hex($binary), 16 + ($i * 8), 8))
-            )['sub'];
-        }
-
-        // Tack on the 'S-' and glue it all together...
-        return 'S-'.$revisionLevel.'-'.$identifierAuthority.implode(
-            preg_filter('/^/', '-', $subAuthorities)
-        );
+        return Utilities::binarySidToString($binary);
     }
 }

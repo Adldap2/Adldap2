81759a5e3b32cf5c9c4fa9658ddf84a19f0daa9e
Steve Bauman <steven_bauman@outlook.com>
2017-08-24 09:48:16 -0400

    Move model traits into new namespace

--- src/Models/Concerns/HasCriticalSystemObject.php
+++ src/Models/Concerns/HasCriticalSystemObject.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Adldap\Models\Traits;
+namespace Adldap\Models\Concerns;
 
 trait HasCriticalSystemObject
 {
--- src/Models/Concerns/HasMemberOf.php
+++ src/Models/Concerns/HasMemberOf.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Adldap\Models\Traits;
+namespace Adldap\Models\Concerns;
 
 use Adldap\Utilities;
 use Adldap\Models\User;

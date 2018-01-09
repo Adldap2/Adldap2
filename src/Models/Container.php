2ef37f71c0954930e4d4924694f19f95a374039f
Steve Bauman <steven_bauman@outlook.com>
2018-01-09 09:30:40 -0500

    Update Container.php

--- src/Models/Container.php
+++ src/Models/Container.php
@@ -19,6 +19,8 @@ class Container extends Entry
     /**
      * Returns the containers system flags integer.
      *
+     * An integer value that contains flags that define additional properties of the class.
+     *
      * @link https://msdn.microsoft.com/en-us/library/ms680022(v=vs.85).aspx
      *
      * @return string

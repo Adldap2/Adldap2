2c29144ab8ba5b54e20ea2eab672116301549e10
Steve Bauman <steven_bauman@outlook.com>
2018-07-24 09:26:29 -0400

    Added getters / setter for the managedBy attribute.
    - Closes #556

--- src/Models/User.php
+++ src/Models/User.php
@@ -593,11 +593,53 @@ class User extends Entry implements Authenticatable
     /**
      * Returns the users mail nickname.
      *
-     * @return string
+     * @return string|null
      */
     public function getMailNickname()
     {
         return $this->getFirstAttribute($this->schema->emailNickname());
+    }
+
+    /**
+     * Returns the distinguished name of the user who is assigned to manage this object.
+     *
+     * @return string|null
+     */
+    public function getManagedBy()
+    {
+        return $this->getFirstAttribute($this->schema->managedBy());
+    }
+
+    /**
+     * Returns the user model of the user who is assigned to manage this object.
+     *
+     * Returns false otherwise.
+     *
+     * @return User|bool
+     */
+    public function getManagedByUser()
+    {
+        if ($dn = $this->getManagedBy()) {
+            return $this->query->newInstance()->findByDn($dn);
+        }
+
+        return false;
+    }
+
+    /**
+     * Sets the user who is assigned to managed this object.
+     *
+     * @param Model|string $dn
+     *
+     * @return $this
+     */
+    public function setManagedBy($dn)
+    {
+        if ($dn instanceof Model) {
+            $dn = $dn->getDn();
+        }
+
+        return $this->setFirstAttribute($this->schema->managedBy(), $dn);
     }
 
     /**

b50521e496f02b6e96b6f37d849d0f86871b7e60
Steve Bauman <steven_bauman@outlook.com>
2019-01-11 13:47:46 -0500

    Added log trait for a log singleton instance.

---
+++ src/Log/LogsInformation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Adldap\Log;
+
+use Psr\Log\LoggerInterface;
+
+trait LogsInformation
+{
+    /**
+     * The logger instance.
+     *
+     * @var LoggerInterface
+     */
+    protected static $logger;
+
+    /**
+     * Get the event dispatcher instance.
+     *
+     * @return LoggerInterface
+     */
+    public static function getLogger()
+    {
+        // If no event dispatcher has been set, well instantiate and
+        // set one here. This will be our singleton instance.
+        if (! isset(static::$logger)) {
+            static::setLogger(new NullLogger());
+        }
+
+        return static::$dispatcher;
+    }
+
+    /**
+     * Set the logger instance.
+     *
+     * @param LoggerInterface $logger
+     *
+     * @return void
+     */
+    public static function setLogger(LoggerInterface $logger)
+    {
+        static::$logger = $logger;
+    }
+
+    /**
+     * Unset the logger instance.
+     *
+     * @return void
+     */
+    public static function unsetLogger()
+    {
+        static::$logger = null;
+    }
+}

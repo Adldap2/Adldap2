f0746ae3d12e8d5571b19d597f091e8938ef7d69
Steve Bauman <steven_bauman@outlook.com>
2019-01-30 22:14:01 -0500

    Added model event log test

--- tests/Log/EventLoggerTest.php
+++ tests/Log/EventLoggerTest.php
@@ -2,9 +2,11 @@
 
 namespace Adldap\Tests\Log;
 
+use Adldap\Models\User;
 use Adldap\Tests\TestCase;
 use Adldap\Log\EventLogger;
 use Adldap\Auth\Events\Event as AuthEvent;
+use Adldap\Models\Events\Event as ModelEvent;
 use Adldap\Connections\ConnectionInterface;
 use Psr\Log\LoggerInterface;
 
@@ -31,5 +33,32 @@ class EventLoggerTest extends TestCase
         $eLogger = new EventLogger($l);
 
         $this->assertNUll($eLogger->auth($e));
+    }
+
+    public function test_model_events_are_logged()
+    {
+        $c = $this->mock(ConnectionInterface::class);
+
+        $b = $this->newBuilder($c);
+
+        $dn = 'cn=John Doe,dc=corp,dc=acme,dc=org';
+
+        $u = new User(['dn' => $dn], $b);
+
+        $l = $this->mock(LoggerInterface::class);
+
+        $eLogger = new EventLogger($l);
+
+        $me = $this->mock(ModelEvent::class);
+
+        $me->shouldReceive('getModel')->once()->andReturn($u);
+
+        $log = "LDAP (ldap://192.168.1.1) - Operation: Mockery_6_Adldap_Models_Events_Event - On: Adldap\Models\User - Distinguished Name: $dn";
+
+        $l->shouldReceive('info')->once()->with($log);
+
+        $c->shouldReceive('getHost')->once()->andReturn('ldap://192.168.1.1');
+
+        $this->assertNUll($eLogger->model($me));
     }
 }
